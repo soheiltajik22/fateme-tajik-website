71115681d4ebdadc013e9d4b32ad1b799ab5a790
Soheil Tajik <soheiltajik1993@gmail.com>
2026-08-22 15:25:29 +0200

    Fix ServiceCard prop error

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -32,7 +32,7 @@ export default function ServicesPage() {
         <div className="container-custom">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
             {services.map((service, i) => (
-              <ServiceCard key={service.id} service={service} index={i} id={service.id} />
+              <ServiceCard key={service.id} service={service} index={i} />
             ))}
           </div>
         </div>
